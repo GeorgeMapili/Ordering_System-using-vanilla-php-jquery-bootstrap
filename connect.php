0e48b85b73960acf0fa4b11fe242bf32ecbcc755
George Mapili <georgemapili322@gmail.com>
2021-02-23 13:39:33 +0800

    added remote mysql connection details

--- connect.php
+++ connect.php
@@ -1,9 +1,15 @@
 <?php
+// Local machine connection
+// $DB_HOST = "localhost";
+// $DB_USER = "root";
+// $DB_NAME = "sadproject";
+// $DB_PASS = "";
 
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_NAME = "sadproject";
-$DB_PASS = "";
+// Remove Db connection
+$DB_HOST = "remotemysql.com";
+$DB_USER = "PgpKEP3m0J";
+$DB_NAME = "PgpKEP3m0J";
+$DB_PASS = "Ez9klv2E4I";
 
 try {
     $con = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
